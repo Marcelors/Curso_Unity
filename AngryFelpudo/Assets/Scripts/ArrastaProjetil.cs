41b09b5934f441691feff37b1ff85eee05d484ca
Marcelo Rodrigues Silva <squal_marcelo@hotmail.com>
2019-08-28 13:16:03 -0300

    Criando linhas do estilingue

--- AngryFelpudo/Assets/Scripts/ArrastaProjetil.cs
+++ AngryFelpudo/Assets/Scripts/ArrastaProjetil.cs
@@ -12,14 +12,57 @@ public class ArrastaProjetil : MonoBehaviour
     Ray raioParaMouse;
     Rigidbody2D rb2D;
     Vector2 velocidadeAnterior;
+
+    public LineRenderer linhaFrente;
+    public LineRenderer linhaTras;
+    Ray raioEstilingueFrente;
+
+    CircleCollider2D collider2D;
+    float medidaCirculo;
     // Start is called before the first frame update
-    void Start()
+
+    private void Awake()
     {
         mola = GetComponent<SpringJoint2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+        collider2D = GetComponent<CircleCollider2D>();
+    }
+
+    void Start()
+    {
         estilingue = mola.connectedBody.transform;
         esticadaMaximaQuadrada = EsticadaMaxima * EsticadaMaxima;
         raioParaMouse = new Ray(estilingue.position, Vector3.zero);
-        rb2D = GetComponent<Rigidbody2D>();
+        raioEstilingueFrente = new Ray(linhaFrente.transform.position, Vector3.zero);
+
+        medidaCirculo = collider2D.radius;
+
+        ConfiguraLinha();
+    }
+
+    void ConfiguraLinha()
+    {
+        linhaFrente.SetPosition(0, linhaFrente.transform.position + Vector3.forward * -0.03f);
+        linhaTras.SetPosition(0, linhaTras.transform.position + Vector3.forward * -0.01f);
+
+        linhaFrente.sortingLayerName = "Frente";
+        linhaTras.sortingLayerName = "Frente";
+
+        linhaFrente.sortingOrder = 3;
+        linhaTras.sortingOrder = 1;
+    }
+
+    void AtualizaLinha()
+    {
+        Vector2 estilingueParaProgetil = transform.position - linhaFrente.transform.position;
+        raioEstilingueFrente.direction = estilingueParaProgetil;
+
+        Vector3 pontoAmarra = raioEstilingueFrente.GetPoint(estilingueParaProgetil.magnitude + medidaCirculo);
+
+        pontoAmarra.z = -0.03f;
+        linhaFrente.SetPosition(1, pontoAmarra);
+        pontoAmarra.z = -0.01f;
+        linhaTras.SetPosition(1, pontoAmarra);
     }
 
     // Update is called once per frame
@@ -42,6 +85,8 @@ public class ArrastaProjetil : MonoBehaviour
             {
                 velocidadeAnterior = rb2D.velocity;
             }
+
+            AtualizaLinha();
         } else
         {
 
@@ -74,6 +119,8 @@ public class ArrastaProjetil : MonoBehaviour
             posicaoMouseMundo = raioParaMouse.GetPoint(EsticadaMaxima);
         }
 
+        posicaoMouseMundo.z = -0.02f;
         transform.position = new Vector3(posicaoMouseMundo.x, posicaoMouseMundo.y, transform.position.z);
+    
     }
 }
---
+++ AngryFelpudo/Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public Transform projetil;
+    public Transform LimiteEsquerdo;
+    public Transform LimiteDireito;
+
+    Vector3 novaPosicao;
+
+    // Update is called once per frame
+    void Update()
+    {
+        novaPosicao = transform.position;
+
+        novaPosicao.x = projetil.position.x;
+        novaPosicao.x = Mathf.Clamp(novaPosicao.x, LimiteEsquerdo.position.x, LimiteDireito.position.x);
+
+        transform.position = novaPosicao;
+    }
+}
