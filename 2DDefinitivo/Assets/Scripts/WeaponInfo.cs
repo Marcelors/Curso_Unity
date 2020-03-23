a83b8cfc2d618d1153bf81d7a119dc63a1bce894
Marcelo <squal_marcelo@hotmail.com>
2020-03-22 22:21:29 -0300

    adicionando equipamento

---
+++ 2DDefinitivo/Assets/Scripts/WeaponInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponInfo : MonoBehaviour
+{
+    public int Damege;
+    public int DamegeType;
+}
